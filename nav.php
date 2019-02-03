28086948438cf74e10d4f200760f3915793b97ee
Albert Kim <mail@albertkim.com>
2019-02-02 19:52:38 -0700

    Improve styles.

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <nav class="<?php if(isset($_SESSION['logged_user'])): ?>loggedin-nav<?php endif; ?>">
 	<div class="corner-title-static">
-		<a href="index.php">flyshare</a>
+		<a href="index.php" class="logo">flyshare</a>
 	</div>
 	<div class="btn-wrapper">
 		<?php 	if(isset($_SESSION['logged_user'])): ?>
