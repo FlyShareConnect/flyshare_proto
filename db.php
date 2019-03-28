500eec630206adf15788af2d4d53adef820f7948
Albert Kim <mail@albertkim.com>
2019-03-27 20:30:55 -0700

    Rm strings.

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
   // Create connection with database for registering
-  $dbhost = "flyshare.live";
-  $dbuser = "imadeitc_albert";
-  $dbpass = "buff2904100";
-  $dbname = "imadeitc_flyshare_db";
+  $dbhost = "";
+  $dbuser = "";
+  $dbpass = "";
+  $dbname = "";
   $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
   // Test if connection occured
   if(mysqli_connect_errno()){
     die("Database connection failed: " .
-      mysqli_connect_error() . 
+      mysqli_connect_error() .
       " (" . mysqli_connect_errno() . ")"
-    );  
+    );
   }
 ?>
